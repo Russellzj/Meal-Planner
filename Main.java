1c691b9b200b39b43220cf714ac5b529bd54ce12
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-04-07 14:48:33 -0400

    create Menu Class and move all functions from Main

--- Main.java
+++ Main.java
@@ -2,34 +2,9 @@ package mealplanner;
 import java.util.Scanner;
 
 public class Main {
-  private String meal;
-  private String mealName;
-  private String[] ingredients;
-
-  public void add() {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Which meal do you want to add (breakfast, lunch, dinner)?");
-    meal = sc.nextLine();
-    System.out.println("Input the meal's name: ");
-    mealName = sc.nextLine();
-    System.out.println("Input the ingredients:");
-    ingredients = sc.nextLine().split(",");
-    printMeal();
-    System.out.println("The meal has been added!");
-  }
-
-  public void printMeal() {
-    System.out.println("Category: " + meal);
-    System.out.println("Name: " + mealName);
-    System.out.println("Ingredients:");
-    for (String ingredient : ingredients) {
-      System.out.println(ingredient);
-    }
-  }
 
     public static void main(String[] args) {
-      Main newMeal = new Main();
+      Menu newMeal = new Menu();
       newMeal.add();
-      newMeal.printMeal();
     }
   }
---
+++ Menu.java
@@ -0,0 +1,30 @@
+package mealplanner;
+
+import java.util.Scanner;
+
+public class Menu {
+    private String meal;
+    private String mealName;
+    private String[] ingredients;
+
+    public void add() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Which meal do you want to add (breakfast, lunch, dinner)?");
+        meal = sc.nextLine();
+        System.out.println("Input the meal's name: ");
+        mealName = sc.nextLine();
+        System.out.println("Input the ingredients:");
+        ingredients = sc.nextLine().split(",");
+        printMeal();
+        System.out.println("The meal has been added!");
+    }
+
+    public void printMeal() {
+        System.out.println("Category: " + meal);
+        System.out.println("Name: " + mealName);
+        System.out.println("Ingredients:");
+        for (String ingredient : ingredients) {
+            System.out.println(ingredient);
+        }
+    }
+}
