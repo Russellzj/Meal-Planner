c4e9366482d692a935b9acbddb52958f36ace827
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-04-15 19:07:10 -0400

    Create loop that asks for commands

--- Main.java
+++ Main.java
@@ -4,8 +4,27 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-      Menu newMeal = new Menu();
-      newMeal.add();
+        Scanner sc = new Scanner(System.in);
+        //Menu newMeal = new Menu();
+        //newMeal.add();
+        boolean continueInput = true; //continues the loop as long as the user does not want to exit
+        while (continueInput) {
+            System.out.println("Would you like to do(add, show exit)?");
+            String userCommand  = sc.next();
+            switch (userCommand) {
+                case "add":
+                    //newMeal.add();
+                    System.out.println("ADD");
+                    break;
+                case "show":
+                    System.out.println("SHOW");
+                    break;
+                case "exit":
+                    continueInput = false;
+                    break;
+                default:
+                    System.out.println("Invalid command");
+            }
+        }
     }
-  }
+}
