03dbe757716999128c50401e3a4f559a0a62534c
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-04-15 19:53:55 -0400

    Create checker for category input

--- Main.java
+++ Main.java
@@ -5,15 +5,15 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //Menu newMeal = new Menu();
-        //newMeal.add();
+        Menu newMeal = new Menu();
+        newMeal.add();
+        newMeal.printMeal();
         boolean continueInput = true; //continues the loop as long as the user does not want to exit
         while (continueInput) {
             System.out.println("Would you like to do(add, show exit)?");
             String userCommand  = sc.next();
             switch (userCommand) {
                 case "add":
-                    //newMeal.add();
                     System.out.println("ADD");
                     break;
                 case "show":
--- Menu.java
+++ Menu.java
@@ -1,27 +1,37 @@
 package mealplanner;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
-    private String meal;
+    private String category;
     private String mealName;
     private String[] ingredients;
 
     public void add() {
 
         Scanner sc = new Scanner(System.in);
+        List<String> mealOptions = List.of("breakfast", "lunch", "dinner");
         System.out.println("Which meal do you want to add (breakfast, lunch, dinner)?");
-        meal = sc.nextLine();
+        String mealChoice;
+        do {
+            mealChoice = sc.next().toLowerCase();
+            if (!mealOptions.contains(mealChoice)) {
+                System.out.println("Wrong meal category! Choose from: breakfast, lunch, dinner.");
+            } else {
+                category = mealChoice;
+            }
+        } while (!mealOptions.contains(mealChoice));
+        category = sc.nextLine().toLowerCase();
         System.out.println("Input the meal's name: ");
         mealName = sc.nextLine();
         System.out.println("Input the ingredients:");
-        ingredients = sc.nextLine().split(",");
-        printMeal();
+        ingredients = sc.nextLine().split(", ");
         System.out.println("The meal has been added!");
     }
 
     public void printMeal() {
-        System.out.println("Category: " + meal);
+        System.out.println("Category: " + category);
         System.out.println("Name: " + mealName);
         System.out.println("Ingredients:");
         for (String ingredient : ingredients) {
