b4c954cfd4861ffdd7dd7c7027e8f14b0659d991
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-04-16 20:46:26 -0400

    create menu ArrayList, create add and show for ArrayList

--- Main.java
+++ Main.java
@@ -1,23 +1,30 @@
 package mealplanner;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Menu newMeal = new Menu();
-        newMeal.add();
-        newMeal.printMeal();
+        //ArrayList to hold all meals created
+        ArrayList<Meal> menu = new ArrayList<>();
         boolean continueInput = true; //continues the loop as long as the user does not want to exit
         while (continueInput) {
             System.out.println("Would you like to do(add, show exit)?");
             String userCommand  = sc.next();
             switch (userCommand) {
+                //Adds Meal to the menu
                 case "add":
-                    System.out.println("ADD");
+                    Meal newMeal = new Meal();
+                    menu.add(newMeal);
+                    menu.get(menu.size()-1).add();
                     break;
                 case "show":
-                    System.out.println("SHOW");
+                    System.out.println();
+                    for (Meal meal : menu) {
+                        System.out.println(meal);
+                        System.out.println();
+                    }
                     break;
                 case "exit":
                     continueInput = false;
@@ -25,6 +32,6 @@ public class Main {
                 default:
                     System.out.println("Invalid command");
             }
-        }
+        } System.out.println("Bye!");
     }
 }
